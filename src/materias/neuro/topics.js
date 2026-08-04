8380fbab0bd621c8e4a1acfe37a107faaf352f9a
agustinfrusto <agustinfrusto@gmail.com>
2026-08-04 17:19:13 -0300

    content: carga banco Neurobiología lote 1 (112 preguntas validadas, materia aún bloqueada)

--- src/materias/neuro/topics.js
+++ src/materias/neuro/topics.js
@@ -1,3 +1,17 @@
-// Topics de Neurobiología. Se completan a medida que se cargan las preguntas.
+// Topics de Neurobiología.
 export const TOPICS = {
+  "organizacion-sn": "Organización del sistema nervioso",
+  "histologia-neuronal": "Neurona: tipos, prolongaciones y organelos",
+  "glia": "Glía (astrocitos, microglía, oligodendrocitos)",
+  "mielina-nervio": "Mielina y nervio periférico",
+  "membrana-potencial": "Potencial de membrana y de acción",
+  "sinapsis": "Sinapsis y transmisión",
+  "receptores-sensoriales": "Receptores sensoriales y transducción",
+  "vias-somatosensoriales": "Vías somatosensoriales y corteza",
+  "reflejos-propiocepcion": "Reflejos, husos y órgano de Golgi",
+  "control-motor": "Control motor y cortezas motoras",
+  "autonomo": "Sistema nervioso autónomo",
+  "medula-espinal": "Médula espinal",
+  "cerebelo": "Cerebelo",
+  "corteza-cerebral": "Corteza cerebral e hipocampo",
 };
